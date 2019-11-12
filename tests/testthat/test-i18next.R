d57a755e2c3584feb9c799c0d135a3bc479bdc05
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2019-11-12 16:40:08 +0000

    Expand testing

--- tests/testthat/test-i18next.R
+++ tests/testthat/test-i18next.R
@@ -6,3 +6,30 @@ test_that("simple usage works", {
   expect_equal(obj$t("hello", language = "en"), "hello world")
   expect_equal(obj$t("hello", language = "fr"), "bonjour le monde")
 })
+
+
+test_that("interpolation", {
+  obj <- i18n(i18n_file("examples/simple.json"))
+  expect_equal(
+    obj$t("interpolate", list(what = "i18next", how = "easy"),
+          language = "en"),
+    "i18next is easy")
+  expect_equal(
+    obj$t("interpolate", list(what = "i18next", how = "facile"),
+          language = "fr"),
+    "i18next est facile")
+})
+
+
+test_that("plurals", {
+  obj <- i18n(i18n_file("examples/simple.json"))
+  expect_equal(obj$t("pluralex1", count = 1), "nose")
+  expect_equal(obj$t("pluralex1", count = 2), "noses")
+  expect_equal(obj$t("pluralex2", count = 1), "sheep")
+  expect_equal(obj$t("pluralex2", count = 2), "sheep")
+
+  expect_equal(obj$t("pluralex1", count = 1, language = "fr"), "nez")
+  expect_equal(obj$t("pluralex1", count = 2, language = "fr"), "nez")
+  expect_equal(obj$t("pluralex2", count = 1, language = "fr"), "mouton")
+  expect_equal(obj$t("pluralex2", count = 2, language = "fr"), "moutons")
+})
