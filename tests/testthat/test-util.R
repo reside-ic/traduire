632f5d14f206675f354c1d18a743a33890f25947
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2019-12-02 12:46:18 +0000

    Test utility

--- tests/testthat/test-util.R
+++ tests/testthat/test-util.R
@@ -19,3 +19,13 @@ test_that("read_input passes json through", {
   dat <- '{"a" : "b"}'
   expect_equal(read_input(dat), jsonlite::unbox(dat))
 })
+
+
+test_that("data_frame does not let factors through", {
+  a <- letters
+  b <- runif(26)
+  expect_is(data_frame(first = a, second = b)$first, "character")
+  expect_identical(
+    data_frame(a, b),
+    data.frame(a, b, stringsAsFactors = FALSE))
+})
