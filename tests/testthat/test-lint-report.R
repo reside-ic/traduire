076c62d7884dcb4df0d93fb780e5782b244a4d3b
Rich FitzJohn <rich.fitzjohn@gmail.com>
2019-12-23 10:49:02 +0000

    Start tests of the reporting

--- tests/testthat/test-lint-report.R
+++ tests/testthat/test-lint-report.R
@@ -9,3 +9,33 @@ test_that("html span", {
                '<span class="myclass" data-tooltip="message">')
   expect_equal(s(FALSE, "message"), '</span>')
 })
+
+
+test_that("report for file", {
+  src <- c(
+    'a <- t_("hello")',
+    'b <- t_("missing")')
+  p <- tempfile()
+  dir.create(p)
+  writeLines(src, file.path(p, "a.R"))
+  obj <- i18n(traduire_file("examples/simple.json"))
+  x <- lint_translations("a.R", obj, root = p)
+  res <- lint_translations_html_report_file(x[[1]])
+
+  ## TODO: this needs work to add an id in order that it is embeddable
+  expect_setequal(names(res), c("tab", "content"))
+  expect_equal(
+    res$tab,
+    '<button class="tablinks" onclick="openTab(event, \'a.R\')">a.R</button>')
+
+  re <- '^<div id="a.R" class="tabcontent">\n<pre>(.+)</pre>\n</div>\\s*$'
+  expect_match(res$content, re)
+  code <- sub(re, "\\1", res$content)
+  spans <- strsplit(trimws(code), "\n", fixed = TRUE)[[1]]
+  expect_equal(
+    spans[[1]],
+    '<span class="line">a &lt;- <span class="valid"><span class="expr">t_("hello")</span></span></span>')
+  expect_equal(
+    spans[[2]],
+    '<span class="line">b &lt;- <span class="expr">t_(<span class="error-missing" data-tooltip="Translation key \'translation:missing\' not found">"missing"</span>)</span></span>')
+})
